7526b24150c15856a6462fb0de4a11d245d170e3
Ray Liu <wooshadyyy@gmail.com>
2023-11-08 01:14:01 -0500

    web app completed

--- src/components/ActiveSlider.jsx
+++ src/components/ActiveSlider.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/free-mode'
-
 import { FreeMode, Pagination } from 'swiper/modules'
-
 import { RxArrowTopRight } from 'react-icons/rx'
 import { ServiceData } from '../constants/constant.js'
-import Image from 'next/image'; 
-
+import Image from 'next/image';
 const ActiveSlider = () => {
     return (
-        <div className="flex items-center justify-center flex-col h-[900px] bg-slate-400">
+        <div className="  sm:text-xs lg:text-lg">
             <Swiper
                 breakpoints={{
                     340: {
                         slidesPerView: 2,
-                        spaceBetween: 5,
+                        spaceBetween: 15,
                     },
                     700: {
                         slidesPerView: 3,
@@ -30,25 +26,25 @@ const ActiveSlider = () => {
                     clickable: true,
                 }}
                 modules={[FreeMode, Pagination]}
-                className="max-w-[90%] lg:max-w-[80%]"
+                className="max-w-[100%] lg:max-w-[100%]"
             >
                 {ServiceData.map((item) => (
-                    <SwiperSlide key={item.title}>
-                        <div className="flex flex-col gap-6 mb-20 group relative  text-white rounded-xl px-6 py-8   md:h-[250px] md:w-[200px]  lg:h-[300px] lg:w-[220px] xl:h-[400px] xl:w-[350px] overflow-hidden cursor-pointer">
+                    <SwiperSlide key={item.title} onClick={() => handleSlideClick(item)}>
+                        <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8   h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
                             <Image
                                 src={item.backgroundImage}
-                                alt={`Image for ${item.title}`} // 提供一个描述性的 alt 属性
+                                alt={`Image for ${item.title}`}
                                 layout="fill"
                                 objectFit="cover"
-                                // className="absolute inset-0"
+
                             />
                             <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
                             <div className="relative flex flex-col gap-3">
                                 <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
                                 <h1 className="text-xl lg:text-2xl">{item.title} </h1>
-                                <p className="lg:text-[18px]">{item.content} </p>
+                                <p className="text-xs lg:text-xl">{item.content} </p>
                             </div>
-                            <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
+                            <RxArrowTopRight  className="absolute    bottom-2 left-2 sm:w-[15px] sm:h-[15px]  lg:w-[35px] lg:h-[35px]  text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
                         </div>
                     </SwiperSlide>
                 ))}
@@ -56,4 +52,13 @@ const ActiveSlider = () => {
         </div>
     );
 };
+function handleSlideClick(item) {
+
+    // Check if the item has a URL
+    if (item.url) {
+        // Open the URL in a new tab
+        window.open(item.url, '_blank');
+    }
+
+}
 export default ActiveSlider;
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-import {
+  import {
     RxCrop,
     RxDesktop,
     RxPencil2,
@@ -6,49 +6,60 @@ import {
     RxRocket,
     RxAccessibility,
   } from "react-icons/rx";
-  
-  import SpaceCity1 from "../assets/SpaceCity1.jpg";
-  import SpaceCity5 from "../assets/SpaceCity5.jpeg";
-  import SpaceCity6 from "../assets/SpaceCity6.jpeg";
-  import SpaceCity7 from "../assets/SpaceCity7.jpeg";
-  import SpaceCity8 from "../assets/SpaceCity8.jpeg";
-  import SpaceCity9 from "../assets/SpaceCity9.jpeg";
+  import {AiFillAndroid} from "react-icons/ai";
+  import {SiCplusplus} from "react-icons/si";
+
+
+  import {AiOutlineMobile} from "react-icons/ai";
+  import { BiLogoSpringBoot, BiLogoVuejs, BiLogoUnity} from "react-icons/bi";
+  import SpaceCity1 from "../../public/assets/SpaceCity1.jpg";
+  import SpaceCity5 from "../../public/assets/SpaceCity5.jpeg";
+  import SpaceCity6 from "../../public/assets/SpaceCity6.jpeg";
+  import SpaceCity7 from "../../public/assets/SpaceCity7.jpeg";
+  import SpaceCity8 from "../../public/assets/SpaceCity8.jpeg";
+  import SpaceCity9 from "../../public/assets/SpaceCity9.jpeg";
   
   export const ServiceData = [
     {
-      icon: RxCrop,
-      title: "Development",
-      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      icon: AiOutlineMobile,
+      title: "ResidentsPad",
+      content: "Tenent-centric Flutter App launched on Google Play Store and Apple App Store.",
       backgroundImage: SpaceCity9,
+      url: "https://play.google.com/store/apps/details?id=com.padmatch.residentspad&hl=en_US&gl=US"
     },
     {
-      icon: RxPencil2,
-      title: "Branding",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: BiLogoSpringBoot,
+      title: "Spring Boot & React App",
+      content: "Full-stack web application for information retrieval built with Spring Boot and React.",
       backgroundImage: SpaceCity1,
+      url: "https://github.com/RayL01/studentdemoBackEnd"
     },
     {
-      icon: RxDesktop,
-      title: "Design",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: BiLogoVuejs,
+      title: "Spring Boot & Vue App",
+      content: "SpringBoot&Vue full stack medical appointment system along with some microservices(Redis, SpringCloud, SpringCloud, Nginx)",
       backgroundImage: SpaceCity6,
+      url: "https://github.com/RayL01/MedicalApptSystem"
     },
     {
-      icon: RxReader,
-      title: "Seo",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: AiFillAndroid,
+      title: "Feed Yourself",
+      content: "This App is based on my android studio project collaborated with my teammates from App Dev Course at NEU",
       backgroundImage: SpaceCity7,
+      url: "https://github.com/hjinlucas/FeedYourself"
     },
     {
-      icon: RxAccessibility,
-      title: "Management",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: SiCplusplus,
+      title: "Breakout Game",
+      content: "Classic Breakout Game built with C++ and OpenGL",
       backgroundImage: SpaceCity5,
+      url: "https://github.com/RayL01/Breakout"
     },
     {
-      icon: RxRocket,
-      title: "Production",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: BiLogoUnity,
+      title: "CrossFire",
+      content: "A 3D FPS game based on Unity. It was a group project finished with my team during undergraduate.",
       backgroundImage: SpaceCity8,
+      url: "https://github.com/RayL01/FPSgame/tree/master"
     },
   ];
